88e3e90f32b92b23d86dad80ec83621e3da9f41c
Gabriel <carloscgmorais@gmail.com>
2025-06-18 22:32:10 -0300

    add sqlite

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,9 +1,14 @@
+from sqlalchemy import create_engine
+
 from fast_zero.models import User
 
 
 def test_create_user():
+    engine = create_engine(
+        'sqlite:////database.db'
+    )
     user = User(
         username='gabriel', email='gabriel@gmail.com', password='senha123'
     )
 
-    assert user.username == 'dunossauro'
+    assert user.username == 'gabriel'
