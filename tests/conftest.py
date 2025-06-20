0d80e3188f3b84f0105ec8b603aab660fdadf3a5
Gabriel <93237267+briellll@users.noreply.github.com>
2025-06-20 13:53:47 -0300

    exercicio 02 aula 04

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from fastapi.testclient import TestClient
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
@@ -8,11 +7,12 @@ from fast_zero.app import app
 from fast_zero.models import table_registry
 
 
-@pytest.fixture()
+@pytest.fixture
 def client():
     return TestClient(app)
 
-@pytest.fixture()
+
+@pytest.fixture
 def session():
     engine = create_engine('sqlite:///:memory:')
     table_registry.metadata.create_all(engine)
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,19 +1,18 @@
-from sqlalchemy import create_engine, select
+from sqlalchemy import select
 
 from fast_zero.models import User
 
 
 def test_create_user(session):
     user = User(
-            username='gabriel', email='gabriel@gmail.com', password='senha123'
+        username='gabriel', email='gabriel@gmail.com', password='senha123'
     )
 
     session.add(user)
     session.commit()
 
     result = session.scalar(
-            select(User).where(User.email == 'gabriel@gmail.com')
+        select(User).where(User.email == 'gabriel@gmail.com')
     )
 
-    assert result.username == 'gabriel' # type:ignore
-
+    assert result.username == 'gabriel'  # type:ignore
