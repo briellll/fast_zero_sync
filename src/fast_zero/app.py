8ff47bb866ba24738a5079580594206ac2f74eb8
Gabriel <carloscgmorais@gmail.com>
2025-06-21 23:39:09 -0300

    melhorando o cadstro DRY

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,12 +1,12 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
+from sqlalchemy import select
 
+from fast_zero.database import get_session
 from fast_zero.models import User
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, USerSchema
-from fast_zero.settings import Settings
+
 
 app = FastAPI()
 
@@ -20,33 +20,31 @@ def read_root():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: USerSchema):
-    engine = create_engine(Settings().DATABASE_URL)  # type: ignore
-
-    with Session(engine) as session:
-        db_user = session.scalar(
+    session = get_session()
+    db_user = session.scalar(
             select(User).where(
                 (User.username == user.username) | (User.email == user.email)
             )
-        )
+    )
 
-        if db_user:
-            if db_user.username == user.username:
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail='Username already exists',
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
                 )
-            elif db_user.email == user.email:
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail='Email already exists',
-                )
-        db_user = User(
-            username=user.username, password=user.password, email=user.email
-        )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
 
-        session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
 
     return db_user
 
---
+++ src/fast_zero/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+
+from fast_zero.settings import Settings
+
+from sqlalchemy.orm import Session
+
+engine = create_engine(Settings().DATABASE_URL) # type:ignore
+
+def get_session() :
+    with Session(engine) as session :
+        return session
